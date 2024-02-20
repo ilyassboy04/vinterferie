e8e8568d2a7d9706d60b2caccc9846249897e6f3
ilyassboy04 <elsah1344@oslomet.no>
2024-02-20 15:56:47 +0100

    initial commit, all the files are made

---
+++ src/main/java/com/example/vinterferie/VinterferieApplication.java
@@ -0,0 +1,13 @@
+package com.example.vinterferie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinterferieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VinterferieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vinterferie/VinterferieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vinterferie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinterferieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
